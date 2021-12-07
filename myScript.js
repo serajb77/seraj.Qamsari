ac00ce67e5e44445549385c291ee258c12a1b348
serab77 <seraj.sepah@gmail.com>
2021-12-07 09:40:06 +0330

    Jquery

--- myScript.js
+++ myScript.js
@@ -1,4 +1,14 @@
-function darkclick() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+$(document).ready(function(){
+    $(".all").hide();
+    $("button").click(function(){
+        if ($(this).text() === 'نمایش مطالب') {
+            $(this).text('پنهان کردن مطالب');
+       }
+       else {
+           $(this).text('نمایش مطالب');
+       }
+       $(".all").toggle(1000);
+
+    });
+   
+    });
